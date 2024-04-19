b2f1c86907ae92967ebb79e878b4620201905154
horyu <28817604+horyu@users.noreply.github.com>
2024-04-19 21:56:22 +0900

    alphanum_less をコメントアウト

--- CudaCropTest/external/alphanum.h
+++ CudaCropTest/external/alphanum.h
@@ -289,19 +289,19 @@ namespace doj
 
   ////////////////////////////////////////////////////////////////////////////
 
-  /**
-     Functor class to compare two objects with the "Alphanum
-     Algorithm". If the objects are no std::string, they must
-     implement "std::ostream operator<< (std::ostream&, const Ty&)".
-  */
-  template<class Ty>
-  struct alphanum_less : public std::binary_function<Ty, Ty, bool>
-  {
-    bool operator()(const Ty& left, const Ty& right) const
-    {
-      return alphanum_comp(left, right) < 0;
-    }
-  };
+  ///**
+  //   Functor class to compare two objects with the "Alphanum
+  //   Algorithm". If the objects are no std::string, they must
+  //   implement "std::ostream operator<< (std::ostream&, const Ty&)".
+  //*/
+  //template<class Ty>
+  //struct alphanum_less : public std::binary_function<Ty, Ty, bool>
+  //{
+  //  bool operator()(const Ty& left, const Ty& right) const
+  //  {
+  //    return alphanum_comp(left, right) < 0;
+  //  }
+  //};
 
 }
 
