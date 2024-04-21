1a117c3fffb61ff99e7091c0bc7b9e2a23456f1c
horyu <28817604+horyu@users.noreply.github.com>
2024-04-21 21:49:49 +0900

    画像を切り抜く

--- CudaCropTest/kernel.cu
+++ CudaCropTest/kernel.cu
@@ -6,27 +6,66 @@
 
 #include "imageIO.h"
 #include "cudaCrop.h"
+#include "cudaMappedMemory.h"
+
+class CudaMemory {
+public:
+	CudaMemory(uchar3* ptr) : ptr_(ptr) {}
+	~CudaMemory() { cudaFree(ptr_); }
+private:
+	uchar3* ptr_;
+};
 
 int main()
 {
-    const char* input_path = R"(C:\Users\owner\source\repos\CudaCropTest\CudaCropTest\images\sample-2000x1500.png)";
-    const char* output_oath = R"(C:\Users\owner\source\repos\CudaCropTest\CudaCropTest\images\test.png)";
+	const char* input_path = R"(C:\Users\owner\source\repos\CudaCropTest\CudaCropTest\images\sample-2000x1500.png)";
+	const char* output_oath = R"(C:\Users\owner\source\repos\CudaCropTest\CudaCropTest\images\test.png)";
 
-    uchar3 *input_image = NULL;
-    int width = 0, height = 0;
+	uchar3* input_image = NULL;
+	uchar3* output_image = NULL;
+	CudaMemory input_memory(input_image), output_memory(output_image);
 
-    // Load image
-    if (!loadImage(input_path, &input_image, &width, &height)) {
-        fprintf(stderr, "loadImage failed!");
+	int width = 0, height = 0;
+
+	// Load image
+	if (!loadImage(input_path, &input_image, &width, &height)) {
+		fprintf(stderr, "loadImage failed!");
 		return 1;
-    }
-    printf("Image loaded: %d x %d\n", width, height);
+	}
+	printf("Image loaded: %d x %d\n", width, height);
 
-    // Save image
-    if (!saveImage(output_oath, input_image, width, height)) {
+	int crop_width = width / 2;
+	int crop_height = height / 3;
+
+	// Allocate output image
+	if (!cudaAllocMapped(&output_image, sizeof(uchar3) * crop_width * crop_height)) {
+		fprintf(stderr, "cudaAllocMapped failed!");
+		return 1;
+	}
+
+	int4 roi = {
+		width / 4,
+		height / 3,
+		width / 4 + crop_width,
+		height / 3 + crop_height
+	};
+	cudaError_t cudaStatus = cudaCrop(
+		input_image,
+		output_image,
+		roi,
+		width,
+		height
+	);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaCrop failed! cudaStatus=%d", cudaStatus);
+		return 1;
+	}
+
+	// Save image
+	if (!saveImage(output_oath, output_image, crop_width, crop_height)) {
 		fprintf(stderr, "saveImage failed!");
-        return 1;
-    }
+		return 1;
+	}
 
-    return 0;
+	return 0;
 }
